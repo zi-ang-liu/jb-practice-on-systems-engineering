bc66044410fccfdb0416cd0d24dd8b9e86f20e46
Ziang LIU <50853393+zi-ang-liu@users.noreply.github.com>
2024-10-04 13:38:30 +0900

    a

--- test.py
+++ test.py
@@ -1,15 +1,45 @@
-# bracket
-# Given a function and distinct initial points, search in the downhill direction (as defined by the initial points) and return three points that bracket the minimum of the function.
+def bracket(f, x1=0, s=1e-2, k=2.0):
+    """
+    bracket function
+
+    Parameters
+    ----------
+    f : function
+        The function to minimize
+    x1 : float
+        Initial guess
+    s : float
+        Step size
+    k : float
+        Expansion factor
+
+    Returns
+    -------
+    tuple
+        A tuple of the form (x1, x2, x3, f1, f2, f3)
+        where x1>x2>x3 and f1>f2<f3
+    """
+    f1 = f(x1)
+    x2 = x1 + s
+    f2 = f(x2)
+    if f2 > f1:
+        x1, x2 = x2, x1
+        f1, f2 = f2, f1
+        s = -s
+    while True:
+        x3 = x2 + s
+        f3 = f(x3)
+        if f3 > f2:
+            return x1, x2, x3, f1, f2, f3
+        x1, x2, x3 = x2, x3, x3 + s
+        f1, f2, f3 = f2, f3, f(x3)
+        s *= k
 
 
 def f(x):
     return x**2
 
 
-def bracket(f, x1, x2, x3):
-    if f(x1) > f(x2) and f(x2) > f(x3):
-        return x1, x2, x3
-    elif f(x1) < f(x2) and f(x2) < f(x3):
-        return x3, x2, x1
-    else:
-        print("Initial points do not bracket the minimum")
+x1, x2, x3, f1, f2, f3 = bracket(f, -1)
+# print with the format string
+print(f"x1={x1}, x2={x2}, x3={x3}, f1={f1}, f2={f2}, f3={f3}")
